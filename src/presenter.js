302f70b6e035ff88edaaef7a5d3d6b7c343f9dc9
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-02-18 23:08:04 -0400

    Implementamos Saludar tomando en cuenta el idioma

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import { saludar, edadp, SaludadorHorario } from "./saludar.js";
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
 const genero_input = document.querySelector("#genero");
+const idioma_input = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -12,5 +13,6 @@ form.addEventListener("submit", (event) => {
     const nombre = nombre_input.value;
     const edad = parseInt(edad_input.value);
     const genero = genero_input.value;
-    div.innerHTML = `<p> ${SaludadorHorario()},${edadp(edad, genero)} ${saludar(nombre)}.</p>`;
+    const idioma = idioma_input.value;
+    div.innerHTML = `<p> ${SaludadorHorario(idioma)}, ${edadp(edad, genero)} ${saludar(nombre)}.</p>`;
 });
--- src/saludar.js
+++ src/saludar.js
@@ -2,26 +2,42 @@
     return nombre;
 }
 
-function edadp(edad, genero) {
-    if (edad > 30) {
-        if (genero == "F") {
-            return "SRA";
-        } else {
-            return "SR";
-        }
-    } else {
-        if (genero == "F") {
-            return "jovencita";
-        } else {
-            return "joven";
-        }
-    }
+function edadp(edad, genero, idioma) {
+  if (idioma == "ES") {
+      if (edad > 30) {
+          if (genero == "F") {
+              return "SRA";
+          } else {
+              return "SR";
+          }
+      } else {
+          if (genero == "F") {
+              return "jovencita";
+          } else {
+              return "joven";
+          }
+      }
+  } else{
+      if (edad > 30) {
+          if (genero == "F") {
+              return "Miss";
+          } else {
+              return "Mister";
+          }
+      } else {
+          if (genero == "F") {
+              return "girl";
+          } else {
+              return "boy";
+          }
+      }
+  }
 }
 
-function SaludadorHorario() {
+function SaludadorHorario(idioma) {
   const hora = new Date().getHours();
   let saludo;
-
+ if (idioma === "ES") {
   if (hora >= 6 && hora < 12) {
       saludo = "Buenos días";
   } else if (hora >= 12 && hora < 19) {
@@ -29,7 +45,15 @@ function SaludadorHorario() {
   } else {
      saludo = "Buenas noches";
   }
-
+ } else if (idioma === "EN") {
+  if (hora >= 6 && hora < 12) {
+    saludo = "Good morning";
+} else if (hora >= 12 && hora < 19) {
+    saludo = "Good afternoon";
+} else {
+   saludo = "Good nigth";
+}
+ }
   return saludo;
 }
 
