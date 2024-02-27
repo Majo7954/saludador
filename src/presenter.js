755852d3f793931143366a66f345659c2794b7fa
Maria Jose Murillo <murillomendoza.mariajose@gmail.com>
2024-02-27 05:21:58 -0400

    Radio Buttons para el genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,10 @@
+// presenter.js
 import { saludar, edadp, SaludadorHorario } from "./saludar.js";
 
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
-const genero_input = document.querySelector("#genero");
+const genero_f_input = document.querySelector("#genero-f"); // Selecciona el botón de radio femenino
+const genero_m_input = document.querySelector("#genero-m"); // Selecciona el botón de radio masculino
 const idioma_input = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
@@ -12,7 +14,12 @@ form.addEventListener("submit", (event) => {
 
     const nombre = nombre_input.value;
     const edad = parseInt(edad_input.value);
-    const genero = genero_input.value;
+    let genero;
+    if (genero_f_input.checked) {
+        genero = genero_f_input.value;
+    } else if (genero_m_input.checked) {
+        genero = genero_m_input.value;
+    }
     const idioma = idioma_input.value;
-    div.innerHTML = `<p> ${SaludadorHorario(idioma)}, ${edadp(edad, genero,idioma)} ${saludar(nombre)}.</p>`;
+    div.innerHTML = `<p> ${SaludadorHorario(idioma)}, ${edadp(edad, genero, idioma)} ${saludar(nombre)}.</p>`;
 });
