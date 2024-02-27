50acef0294d1dbd6b2343b8dfe6345f8c3cb8e1d
Maria Jose Murillo <murillomendoza.mariajose@gmail.com>
2024-02-27 05:01:02 -0400

    Correccion y aumento de que los campos sean requeridos

--- src/saludar.js
+++ src/saludar.js
@@ -1,60 +1,44 @@
-  function saludar(nombre) {
+function saludar(nombre) {
     return nombre;
 }
 
 function edadp(edad, genero, idioma) {
-  if (idioma == "ES") {
-      if (edad > 30) {
-          if (genero == "F") {
-              return "SRA";
-          } else {
-              return "SR";
-          }
-      } else {
-          if (genero == "F") {
-              return "jovencita";
-          } else {
-              return "joven";
-          }
-      }
-  } else if (idioma === "IN") {
-      if (edad > 30) {
-          if (genero == "F") {
-              return "Miss";
-          } else {
-              return "Mister";
-          }
-      } else {
-          if (genero == "F") {
-              return "girl";
-          } else {
-              return "boy";
-          }
-      }
-  }
+    if (idioma === "ES") {
+        if (edad > 30) {
+            return genero === "F" ? "SRA" : "SR";
+        } else {
+            return genero === "F" ? "jovencita" : "joven";
+        }
+    } else if (idioma === "EN") {
+        if (edad > 30) {
+            return genero === "F" ? "Miss" : "Mister";
+        } else {
+            return genero === "F" ? "girl" : "boy";
+        }
+    }
 }
 
 function SaludadorHorario(idioma) {
-  const hora = new Date().getHours();
-  let saludo;
- if (idioma === "ES") {
-  if (hora >= 6 && hora < 12) {
-      saludo = "Buenos días";
-  } else if (hora >= 12 && hora < 19) {
-      saludo = "Buenas tardes";
-  } else {
-     saludo = "Buenas noches";
-  }
- } else if (idioma === "IN") {
-  if (hora >= 6 && hora < 12) {
-    saludo = "Good morning";
-} else if (hora >= 12 && hora < 19) {
-    saludo = "Good afternoon";
-} else {
-   saludo = "Good night";
-}
- }
-  return saludo;
+    const hora = new Date().getHours();
+    let saludo;
+    if (idioma === "ES") {
+        if (hora >= 6 && hora < 12) {
+            saludo = "Buenos días";
+        } else if (hora >= 12 && hora < 19) {
+            saludo = "Buenas tardes";
+        } else {
+            saludo = "Buenas noches";
+        }
+    } else if (idioma === "EN") {
+        if (hora >= 6 && hora < 12) {
+            saludo = "Good morning";
+        } else if (hora >= 12 && hora < 19) {
+            saludo = "Good afternoon";
+        } else {
+            saludo = "Good night";
+        }
+    }
+    return saludo;
 }
 
 export { saludar, edadp, SaludadorHorario };
